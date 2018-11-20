dda71b478d0414440e8cd658143d5acf73650f30
malzano <i.love.u.baby2353@gmail.com>
2018-11-20 19:19:48 +0700

    initialize All Town

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,41 @@ public class Main {
 		Town Bu4 = new Town("Blue4",1);
 		Town Bu5 = new Town("Blue5",1);
 		Town Bu6 = new Town("Blue6",1);
+		Town Bu7 = new Town("Blue7",1);
+		Town Bu8 = new Town("Blue8",1);
+		Town Bu9 = new Town("Blue9",1);
+		Town Bu10 = new Town("Blue10",1);
+		Town Bu11 = new Town("Blue11",1);
+		Town Bu12 = new Town("Blue12",1);
+		Town Bu13 = new Town("Blue13",1);
+		//next is yellow
+		Town Ye1 = new Town("Yellow1",2);
+		Town Ye2 = new Town("Yellow2",2);
+		Town Ye3 = new Town("Yellow3",2);
+		Town Ye4 = new Town("Yellow4",2);
+		Town Ye5 = new Town("Yellow5",2);
+		Town Ye6 = new Town("Yellow6",2);
+		Town Ye7 = new Town("Yellow7",2);
+		Town Ye8 = new Town("Yellow8",2);
+		Town Ye9 = new Town("Yellow9",2);
+		Town Ye10 = new Town("Yellow10",2);
+		Town Ye11 = new Town("Yellow11",2);
+		Town Ye12= new Town("Yellow12",2);
+		Town Ye13 = new Town("Yellow13",2);
+		//next is Black
+		Town Bk1 = new Town("Black1",3);
+		Town Bk2 = new Town("Black2",3);
+		Town Bk3 = new Town("Black3",3);
+		Town Bk4 = new Town("Black4",3);
+		Town Bk5 = new Town("Black5",3);
+		Town Bk6 = new Town("Black6",3);
+		Town Bk7 = new Town("Black7",3);
+		Town Bk8 = new Town("Black8",3);
+		Town Bk9 = new Town("Black9",3);
+		Town Bk10 = new Town("Blac10",3);
+		Town Bk11 = new Town("Black11",3);
+		Town Bk12 = new Town("Black12",3);
+		Town Bk13 = new Town("Black13",3);
 	}
 
 }
